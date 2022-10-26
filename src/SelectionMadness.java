eac82033d538954455ce17c7a8fa45f5b003f54b
student <bonniec15@nycstudents.net>
2022-10-26 10:38:05 -0400

    implementing methods

--- src/SelectionMadness.java
+++ src/SelectionMadness.java
@@ -1,2 +1,51 @@
-public class SelectionMadness {
+public class SelectionMadness
+{
+    // No instance variables //
+
+    // no-parameter constructor with "empty" body since no instance variables
+    public SelectionMadness() { }
+
+    /* Simulates randomly flipping a fair coin and returning true for heads and
+       false for tails; hint: this method should use the Math.random() method to
+       generate a random number.  This method returns true (heads) 50% of the
+       time and false (tails) 50% of the time, on average.
+     */
+    public boolean flipCoin() {
+        int num = (int) (Math.random() * 1) + 1;
+        if (num == 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /* Returns the largest of three provided integers: num1, num2, or num3
+     */
+    public int largest(int num1, int num2, int num3) {
+        int greatest = 0;
+
+        if (num1 >= num2) {
+            greatest = num1;
+        } else {
+            greatest = num2;
+        }
+        if(greatest <= num3) {
+            greatest =num3;
+        }
+            return greatest;
+    }
+
+    /* Returns true if the three provided lengths create a right triangle, in
+       other words, a2 + b2 = c2, where c is the longest side; returns false
+       Otherwise (hint: use the largest() method that you wrote above!).
+       Be careful -- this is a bit more challenging than it seems!  Consider if
+       side1 = 3, side2 = 4, and side3 = 5 vs. if side1 = 5, side2 = 4, and
+       side3 = 3 -- will your code work regardless of which side is the longest
+       of the three?
+     */
+    public boolean rightTriangle(int side1, int side2, int side3) {
+        return true;
+    }
+
 }
+
